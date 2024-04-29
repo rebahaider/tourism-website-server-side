e613e860227c81f8df3294156f09e2a50cc577e9
rebahaider <rebahaiderema@gmail.com>
2024-04-30 01:43:23 +0600

    doing post and get method

--- index.js
+++ index.js
@@ -36,12 +36,33 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const spotCollection = client.db("spotDB").collection('spot');
+
+        app.get('/addTourists', async (req, res) => {
+            const cursor = spotCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        app.post('/addTourists', async (req, res) => {
+            const newSpot = req.body;
+            console.log(newSpot);
+
+            const result = await spotCollection.insertOne(newSpot);
+            res.send(result);
+        })
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
